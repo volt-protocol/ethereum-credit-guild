658e0437c79b0de02dd6f42880b4114719cc3f6e
Kirk Hutchison <kirk@voltprotocol.io>
2023-03-19 19:37:34 -0700

    start to work on some code in foundry

--- src/Loan.sol
+++ src/Loan.sol
@@ -1,43 +1,87 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "lib/solmate/src/tokens/ERC20.sol";
 
 contract Loan {
 
     struct LendingTerm {
-        uint256 denomination; // the borrowed token and denomination for interest & call fee
-        uint256 collateralRatio; // the borrow limit, expressed in terms of the number of borrow tokens per collateral tokens
+        address borrowToken; // the borrowed token and denomination for interest & call fee
+        address collateralToken; // the token accepted as collateral
+        uint256 collateralRatio; // the borrow limit, expressed in terms of the number of borrow tokens per collateral token
         // this ratio is also used as the liquidation threshold
-        uint256 interestRate; // the interest rate per block
-        uint256 callFee; // the fee users must pay to call the loan
+        uint256 interestRate; // the interest rate in terms of the borrow token, bips annual
+        uint256 callFee; // the fee users must pay to call the loan in terms of the borrow token, bips
     }
 
-    // per account per lending token, how much is available to borrow
-    mapping(address => mapping(address => uint256)) public userAvailableBalance;
-
-    // per account per collateral token, the lending terms they are willing to offer
-    mapping(address => mapping(address => LendingTerm)) public availableTerms;
-
-    // this function just deposits a certain amount of a given lending token
-    // defining lending terms happens separately
-    function lendTokens(address token, uint256 amount) public {
-        ERC20(token).transferFrom(msg.sender, address(this), amount);
-        userAvailableBalance[msg.sender] += amount;
+    struct DebtPosition {
+        address lender;
+        address borrower;
+        uint256 debtBalance;
+        uint256 terms;
+        uint256 originationTime;
+        bool isCalled;
     }
 
-    function withdrawTokens(address token, uint256 amount) public {
-        ERC20(token).transfer(msg.sender, amount);
-        require(userAvailableBalance[msg.sender] >=0);
+    LendingTerm[] public availableTerms;
+    DebtPosition[] public debtPositions;
+
+    // keep track of which terms lenders have agreed to
+    mapping(address => mapping(uint256 => bool)) public lenderTerms;
+
+    // anyone can define a new LendingTerm if they pay the gas cost
+    // inputs:
+    // the token to lend/borrow
+    // the token to use as collateral
+    // the number of borrowable tokens per collateral token
+    // interest in terms of bips annual
+    // call fee in terms of bips
+    function defineTerms(address borrowToken, address collateralToken, uint256 collateralRatio, uint256 interestRate, uint256 callFee) public {
+        availableTerms.push(
+            LendingTerm(
+                {
+                    borrowToken: borrowToken,
+                    collateralToken: collateralToken,
+                    collateralRatio: collateralRatio,
+                    interestRate: interestRate,
+                    callFee: callFee
+                }
+            )
+        );
     }
 
-    uint256 public number;
+    // inputs:
+    // array index of the desired terms
+    // the lender to borrow from
+    // the amount of collateral to deposit
+    // how many tokens to borrow
+    function borrowTokens(uint256 terms, address lender, uint256 collateralAmount, uint256 borrowAmount) public {
+        // TODO safemath, require that the proposed loan meets the loan terms
+        require(availableTerms[terms].collateralRatio * collateralAmount <= borrowAmount);
+        // require that the lender has approved these terms
+        require(lenderTerms[lender][terms]);
 
-    function setNumber(uint256 newNumber) public {
-        number = newNumber;
+        // record the new debt position
+        debtPositions.push(
+            DebtPosition(
+                {
+                    lender: lender,
+                    borrower: msg.sender,
+                    debtBalance: borrowAmount,
+                    terms: terms,
+                    originationTime: block.timestamp,
+                    isCalled: false
+                }
+            )
+        );
+
+        // pull collateral tokens from the borrower
+        ERC20(availableTerms[terms].collateralToken).transferFrom(msg.sender, address(this), collateralAmount);
+        // pull debt tokens from lender and send to the borrower
+        ERC20(availableTerms[terms].borrowToken).transferFrom(lender, msg.sender, borrowAmount);
     }
 
-    function increment() public {
-        number++;
+    function getBorrowToken(uint256 id) external view returns (address) {
+        return availableTerms[id].borrowToken;
     }
 }
--- test/Loan.t.sol
+++ test/Loan.t.sol
@@ -3,22 +3,26 @@ pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
 import "../src/Loan.sol";
+import "lib/solmate/src/tokens/ERC20.sol";
+
+contract testERC20A is ERC20("Circle Dollar", "USDC", 8) {}
+contract testERC20B is ERC20("Hexagon Bond Token", "HBT", 18) {}
 
 contract LoanTest is Test {
     Loan public loan;
+    testERC20A public myLendToken;
+    testERC20B public myCollateralToken;
+
 
     function setUp() public {
         loan = new Loan();
-        loan.setNumber(0);
+        myLendToken = new testERC20A();
+        myCollateralToken = new testERC20B();
     }
 
-    function testIncrement() public {
-        loan.increment();
-        assertEq(loan.number(), 1);
-    }
-
-    function testSetNumber(uint256 x) public {
-        loan.setNumber(x);
-        assertEq(loan.number(), x);
+    // test that a new loan term can be defined and that the borrow token is encoded correctly
+    function testDefineTerms() public {
+        loan.defineTerms(address(myLendToken), address(myCollateralToken), 1000000000, 450, 50);
+        assertEq(loan.getBorrowToken(0), address(myLendToken));
     }
 }
