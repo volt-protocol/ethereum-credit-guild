c53f27f8d64bb7ba88c76c85d6ceb4a8c02a73fc
Kirk Hutchison <kirk@voltprotocol.io>
2023-03-20 13:08:02 -0700

    borrow + repay borrow is working

--- src/Loan.sol
+++ src/Loan.sol
@@ -11,19 +11,21 @@ contract Loan {
         uint256 collateralRatio; // the borrow limit, expressed in terms of the number of borrow tokens per collateral token
         // this ratio is also used as the liquidation threshold
         // TODO: safemath so this can handle collateral tokens with small unit value
-        uint256 interestRate; // the interest rate in terms of the borrow token, bips annual
+        uint256 interestRate; // the interest rate in terms of percent annual
         uint256 callFee; // the fee users must pay to call the loan
         // expressed in terms of the divisor to apply to the total debt
         // 20 would imply a 5% call fee, 50 would imply a 2% call fee, and so on
+        uint256 callPeriod; // the time in blocks that the borrower has to repay the loan before the collateral is seized by the lender
     }
 
     struct DebtPosition {
         address lender;
         address borrower;
         uint256 debtBalance;
+        uint256 underlyingBalance;
         uint256 terms;
         uint256 originationTime;
-        bool isCalled;
+        uint256 callBlock;
     }
 
     LendingTerm[] public availableTerms;
@@ -40,18 +42,27 @@ contract Loan {
     // TODO: safemath so this can handle collateral tokens with small unit value
     // interest in terms of bips annual
     // call fee in terms of bips
-    function defineTerms(address borrowToken, address collateralToken, uint256 collateralRatio, uint256 interestRate, uint256 callFee) public {
-        availableTerms.push(
-            LendingTerm(
-                {
-                    borrowToken: borrowToken,
-                    collateralToken: collateralToken,
-                    collateralRatio: collateralRatio,
-                    interestRate: interestRate,
-                    callFee: callFee
-                }
-            )
-        );
+    // call period in blocks
+    function defineTerms(
+        address borrowToken,
+        address collateralToken,
+        uint256 collateralRatio,
+        uint256 interestRate,
+        uint256 callFee,
+        uint256 callPeriod)
+        public {
+            availableTerms.push(
+                LendingTerm(
+                    {
+                        borrowToken: borrowToken,
+                        collateralToken: collateralToken,
+                        collateralRatio: collateralRatio,
+                        interestRate: interestRate,
+                        callFee: callFee,
+                        callPeriod: callPeriod
+                    }
+                )
+            );
     }
 
     function approveTerms(uint256 terms) public {
@@ -76,9 +87,10 @@ contract Loan {
                     lender: lender,
                     borrower: msg.sender,
                     debtBalance: borrowAmount,
+                    underlyingBalance: 0,
                     terms: terms,
                     originationTime: block.timestamp,
-                    isCalled: false
+                    callBlock: 0 // a call block of zero indicates that the loan has not been called
                 }
             )
         );
@@ -96,7 +108,48 @@ contract Loan {
             address(this),
             debtPositions[index].debtBalance / availableTerms[debtPositions[index].terms].callFee
         );
-        debtPositions[index].isCalled = true;
+        require(debtPositions[index].callBlock == 0); // require that the loan has not already been called
+        require(debtPositions[index].debtBalance > 0); // require there is a nonzero debt balance
+        debtPositions[index].callBlock = block.number; // set the call block to the current block
+        debtPositions[index].debtBalance -= debtPositions[index].debtBalance / availableTerms[debtPositions[index].terms].callFee; // deduct the call fee from the borrower's debt
+    }
+
+    // anyone can repay a debt on behalf of the borrower
+    // for now and for simplicity, full repayment only
+    function repayBorrow(uint256 index) public {
+
+        // first, compute the interest owed
+        // the interest rate is expressed in terms of borrow tokens per year per token borrowed
+        uint256 amountToPay = 
+            debtPositions[index].debtBalance +
+            (
+                debtPositions[index].debtBalance * // the amount borrowed
+                availableTerms[debtPositions[index].terms].interestRate * // times the annual rate
+                (block.timestamp - debtPositions[index].originationTime) / 3153600000 // get the amount of time elapsed since borrow and convert to years
+            );
+
+        ERC20(availableTerms[debtPositions[index].terms].borrowToken).transferFrom(
+            msg.sender,
+            address(this),
+            amountToPay);
+        debtPositions[index].underlyingBalance += amountToPay;
+        debtPositions[index].debtBalance = 0;
+        debtPositions[index].callBlock = 0; // the loan can no longer be called or collateral seized once the debt is repaid
+    }
+
+    function getDebtBalance(uint256 id) external view returns (uint256) {
+        return(
+            debtPositions[id].debtBalance +
+            (
+                debtPositions[id].debtBalance * // the amount borrowed
+                availableTerms[debtPositions[id].terms].interestRate * // times the annual rate
+                (block.timestamp - debtPositions[id].originationTime) / 3153600000 // get the amount of time elapsed since borrow and convert to years
+            )
+        );
+    }
+
+    function getCallBlock(uint256 id) external view returns (uint256) {
+        return debtPositions[id].callBlock;
     }
 
     function getBorrowToken(uint256 id) external view returns (address) {
--- test/Loan.t.sol
+++ test/Loan.t.sol
@@ -7,9 +7,10 @@ import "lib/solmate/src/tokens/ERC20.sol";
 
 contract testERC20 is ERC20 {
 
-    constructor(string memory name, string memory symbol, uint8 decimals, address user, uint256 mintAmount) ERC20(name, symbol, decimals) {
-        _mint(user, mintAmount);
-        totalSupply += mintAmount;
+    constructor(string memory name, string memory symbol, uint8 decimals, address user1, address user2, uint256 mintAmount) ERC20(name, symbol, decimals) {
+        _mint(user1, mintAmount);
+        _mint(user2, mintAmount);
+        totalSupply += (mintAmount * 2);
     }
 }
 
@@ -24,9 +25,9 @@ contract LoanTest is Test {
 
     function setUp() public {
         myLoan = new Loan();
-        myLendToken = new testERC20("DAI Stablecoin", "DAI", 18, myLender, 100000000000000000000000);
-        myCollateralToken = new testERC20("Wrapped Ether", "WETH", 18, myBorrower, 100000000000000000000000);
-        myLoan.defineTerms(address(myLendToken), address(myCollateralToken), 98, 425, 50);
+        myLendToken = new testERC20("DAI Stablecoin", "DAI", 18, myLender, myBorrower, 100000000000000000000000);
+        myCollateralToken = new testERC20("Wrapped Ether", "WETH", 18, myLender, myBorrower, 100000000000000000000000);
+        myLoan.defineTerms(address(myLendToken), address(myCollateralToken), 98, 4, 50, 100);
     }
 
     // test that a new loan term can be defined and that the borrow token is encoded correctly
@@ -43,7 +44,40 @@ contract LoanTest is Test {
         vm.startPrank(myBorrower);
         myCollateralToken.approve(address(myLoan), 100000000000000000000000);
         myLoan.borrowTokens(0, myLender, 100000000000000000000000, 100000000000000000000000);
-        vm.stopPrank;
-        assertEq(myLendToken.balanceOf(myBorrower), 100000000000000000000000);
+        vm.stopPrank();
+        assertEq(myLendToken.balanceOf(myBorrower), 200000000000000000000000, "Borrow token balance is not correct.");
+        assertEq(myCollateralToken.balanceOf(myBorrower), 0, "Collateral token balance is not correct.");
+    }
+
+    function testCallLoan() public {
+        vm.startPrank(myLender);
+        myLendToken.approve(address(myLoan), 100000000000000000000000);
+        myLoan.approveTerms(0);
+        vm.stopPrank();
+        vm.startPrank(myBorrower);
+        myCollateralToken.approve(address(myLoan), 100000000000000000000000);
+        myLoan.borrowTokens(0, myLender, 10000000000000000000000, 10000000000000000000000);
+        vm.stopPrank();
+        vm.startPrank(myLender);
+        uint256 myBlock = block.number;
+        myLoan.callPosition(0);
+        vm.stopPrank();
+        assertEq(myLoan.getCallBlock(0), myBlock);
+    }
+
+    function testRepayBorrow() public {
+        uint256 myExpectedBalance = 100000000000000000000000 - 400000000000000000;
+        vm.startPrank(myLender);
+        myLendToken.approve(address(myLoan), 100000000000000000000000);
+        myLoan.approveTerms(0);
+        vm.stopPrank();
+        vm.startPrank(myBorrower);
+        myCollateralToken.approve(address(myLoan), 100000000000000000000000);
+        myLendToken.approve(address(myLoan), 100000000000000000000000);
+        myLoan.borrowTokens(0, myLender, 10000000000000000000000, 10000000000000000000);
+        vm.warp(block.timestamp + 31536000);
+        myLoan.repayBorrow(0);
+        console.log(myLendToken.balanceOf(myBorrower));
+        assertEq(myLendToken.balanceOf(myBorrower), myExpectedBalance);
     }
 }
