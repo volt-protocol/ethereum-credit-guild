65908bc70b122a6fead92b501be9b7a26712cfe2
Erwan Beauvois <eswak@users.noreply.github.com>
2023-06-27 17:38:38 +0200

    Add USDC LendingTerm with automatic forgiveness if blacklisted

--- test/proposals/Proposal_0.sol
+++ test/proposals/Proposal_0.sol
@@ -11,53 +11,94 @@ import {VoltVetoGovernor} from "@src/governance/VoltVetoGovernor.sol";
 import {RateLimitedCreditMinter} from "@src/rate-limits/RateLimitedCreditMinter.sol";
 import {CreditToken} from "@src/tokens/CreditToken.sol";
 import {GuildToken} from "@src/tokens/GuildToken.sol";
+import {AuctionHouse} from "@src/loan/AuctionHouse.sol";
+import {LendingTerm} from "@src/loan/LendingTerm.sol";
+import {LendingTermUSDC} from "@src/loan/LendingTermUSDC.sol";
 
 contract Proposal_0 is Proposal {
     string public name = "Proposal_0";
 
     function deploy(Addresses addresses) public {
-        Core core = new Core();
-        CreditToken credit = new CreditToken(address(core));
-        GuildToken guild = new GuildToken(
-            address(core),
-            address(credit),
-            7 days, // gaugeCycleLength,
-            1 days // incrementFreezeWindow
-        );
-        VoltTimelockController timelock = new VoltTimelockController(
-            address(core),
-            3 days
-        );
-        VoltGovernor governor = new VoltGovernor(
-            address(core),
-            address(timelock),
-            address(guild),
-            0, // initialVotingDelay
-            7000 * 3, // initialVotingPeriod (~7000 blocks/day)
-            2_500_000 ether, // initialProposalThreshold
-            10_000_000 ether // initialQuorum
-        );
-        VoltVetoGovernor vetoGovernor = new VoltVetoGovernor(
-            address(core),
-            address(timelock),
-            address(credit),
-            2_500_000 ether // initialQuorum
-        );
-        RateLimitedCreditMinter rateLimitedCreditMinter = new RateLimitedCreditMinter(
-            address(core),
-            address(credit),
-            0, // maxRateLimitPerSecond
-            0, // rateLimitPerSecond
-            2_000_000 ether // bufferCap
-        );
+        // Core
+        {
+            Core core = new Core();
+            addresses.addMainnet("CORE", address(core));
+        }
 
-        addresses.addMainnet("CORE", address(core));
-        addresses.addMainnet("ERC20_CREDIT", address(credit));
-        addresses.addMainnet("ERC20_GUILD", address(guild));
-        addresses.addMainnet("TIMELOCK", address(timelock));
-        addresses.addMainnet("GOVERNOR", address(governor));
-        addresses.addMainnet("VETO_GOVERNOR", address(vetoGovernor));
-        addresses.addMainnet("RATE_LIMITED_CREDIT_MINTER", address(rateLimitedCreditMinter));
+        // Tokens & minting
+        {
+            CreditToken credit = new CreditToken(addresses.mainnet("CORE"));
+            GuildToken guild = new GuildToken(
+                addresses.mainnet("CORE"),
+                address(credit),
+                7 days, // gaugeCycleLength,
+                1 days // incrementFreezeWindow
+            );
+            RateLimitedCreditMinter rateLimitedCreditMinter = new RateLimitedCreditMinter(
+                addresses.mainnet("CORE"),
+                address(credit),
+                0, // maxRateLimitPerSecond
+                0, // rateLimitPerSecond
+                2_000_000 ether // bufferCap
+            );
+            addresses.addMainnet("ERC20_CREDIT", address(credit));
+            addresses.addMainnet("ERC20_GUILD", address(guild));
+            addresses.addMainnet("RATE_LIMITED_CREDIT_MINTER", address(rateLimitedCreditMinter));
+        }
+
+        // Governance
+        {
+            VoltTimelockController timelock = new VoltTimelockController(
+                addresses.mainnet("CORE"),
+                3 days
+            );
+            VoltGovernor governor = new VoltGovernor(
+                addresses.mainnet("CORE"),
+                address(timelock),
+                addresses.mainnet("ERC20_GUILD"),
+                0, // initialVotingDelay
+                7000 * 3, // initialVotingPeriod (~7000 blocks/day)
+                2_500_000 ether, // initialProposalThreshold
+                10_000_000 ether // initialQuorum
+            );
+            VoltVetoGovernor vetoGovernor = new VoltVetoGovernor(
+                addresses.mainnet("CORE"),
+                address(timelock),
+                addresses.mainnet("ERC20_CREDIT"),
+                2_500_000 ether // initialQuorum
+            );
+            addresses.addMainnet("TIMELOCK", address(timelock));
+            addresses.addMainnet("GOVERNOR", address(governor));
+            addresses.addMainnet("VETO_GOVERNOR", address(vetoGovernor));
+        }
+
+        // Terms & Auction House
+        {
+            AuctionHouse auctionHouse = new AuctionHouse(
+                addresses.mainnet("CORE"),
+                addresses.mainnet("ERC20_GUILD"),
+                addresses.mainnet("RATE_LIMITED_CREDIT_MINTER"),
+                addresses.mainnet("ERC20_CREDIT")
+            );
+            LendingTermUSDC termUsdc1 = new LendingTermUSDC(
+                addresses.mainnet("CORE"),
+                addresses.mainnet("ERC20_GUILD"),
+                address(auctionHouse),
+                addresses.mainnet("RATE_LIMITED_CREDIT_MINTER"),
+                addresses.mainnet("ERC20_CREDIT"),
+                LendingTerm.LendingTermParams({
+                    collateralToken: addresses.mainnet("ERC20_USDC"),
+                    creditPerCollateralToken: 0.98e30, // 0.98 CREDIT per USDC collateral + 12 decimals of correction
+                    interestRate: 0.02e18, // 2%
+                    callFee: 0.02e18, // 2%
+                    callPeriod: 600, // 10 minutes
+                    hardCap: 2_000_000e18, // max 2M CREDIT issued
+                    ltvBuffer: 0.95e18 // 95%
+                })
+            );
+            addresses.addMainnet("AUCTION_HOUSE_1", address(auctionHouse));
+            addresses.addMainnet("TERM_USDC_1", address(termUsdc1));
+        }
     }
 
     function afterDeploy(Addresses addresses, address deployer) public {
