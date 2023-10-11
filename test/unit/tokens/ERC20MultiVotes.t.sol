cc5597ffb1c7f26f197a934fb09beda541008abf
Elliot <elliotfriedman3@gmail.com>
2023-10-11 12:38:58 -0700

    add containsDelegate assertions
    Signed-off-by: Elliot <elliotfriedman3@gmail.com>

--- test/unit/tokens/ERC20MultiVotes.t.sol
+++ test/unit/tokens/ERC20MultiVotes.t.sol
@@ -46,6 +46,8 @@ contract ERC20MultiVotesUnitTest is Test {
         assertEq(token.delegates(delegate1).length, 0);
         assertEq(token.delegatesVotesCount(delegate1, delegate1), 0);
         assertEq(token.delegatesVotesCount(delegate1, delegate2), 0);
+        assertFalse(token.containsDelegate(delegate1, delegate2));
+        assertFalse(token.containsDelegate(delegate1, delegate1));
 
         vm.prank(delegate1);
         token.incrementDelegation(delegate1, 50);
@@ -54,6 +56,7 @@ contract ERC20MultiVotesUnitTest is Test {
         assertEq(token.delegates(delegate1)[0], delegate1);
         assertEq(token.delegatesVotesCount(delegate1, delegate1), 50);
         assertEq(token.delegatesVotesCount(delegate1, delegate2), 0);
+        assertTrue(token.containsDelegate(delegate1, delegate1));
 
         vm.prank(delegate1);
         token.incrementDelegation(delegate2, 50);
@@ -63,6 +66,7 @@ contract ERC20MultiVotesUnitTest is Test {
         assertEq(token.delegates(delegate1)[1], delegate2);
         assertEq(token.delegatesVotesCount(delegate1, delegate1), 50);
         assertEq(token.delegatesVotesCount(delegate1, delegate2), 50);
+        assertTrue(token.containsDelegate(delegate1, delegate2));
     }
 
     /// @notice test delegating different delegatees 8 times by multiple users and amounts
@@ -76,20 +80,35 @@ contract ERC20MultiVotesUnitTest is Test {
         unchecked {
             uint224 sum;
             for (uint256 i = 0; i < 8; i++) {
-                vm.assume(sum + amounts[i] >= sum && from[i] != address(0) && delegates[i] != address(0));
+                vm.assume(
+                    sum + amounts[i] >= sum &&
+                        from[i] != address(0) &&
+                        delegates[i] != address(0)
+                );
                 sum += amounts[i];
 
                 token.mint(from[i], amounts[i]);
 
                 uint256 userDelegatedBefore = token.userDelegatedVotes(from[i]);
-                uint256 delegateVotesBefore = token.delegatesVotesCount(from[i], delegates[i]);
+                uint256 delegateVotesBefore = token.delegatesVotesCount(
+                    from[i],
+                    delegates[i]
+                );
                 uint256 votesBefore = token.getVotes(delegates[i]);
 
                 vm.prank(from[i]);
                 token.incrementDelegation(delegates[i], amounts[i]);
-                require(token.delegatesVotesCount(from[i], delegates[i]) == delegateVotesBefore + amounts[i]);
-                require(token.userDelegatedVotes(from[i]) == userDelegatedBefore + amounts[i]);
-                require(token.getVotes(delegates[i]) == votesBefore + amounts[i]);
+                require(
+                    token.delegatesVotesCount(from[i], delegates[i]) ==
+                        delegateVotesBefore + amounts[i]
+                );
+                require(
+                    token.userDelegatedVotes(from[i]) ==
+                        userDelegatedBefore + amounts[i]
+                );
+                require(
+                    token.getVotes(delegates[i]) == votesBefore + amounts[i]
+                );
             }
         }
     }
@@ -185,8 +204,13 @@ contract ERC20MultiVotesUnitTest is Test {
         uint256 expected = newDelegatee == address(0) ? 0 : mintAmount;
         uint256 expectedFree = newDelegatee == address(0) ? mintAmount : 0;
 
-        require(oldDelegatee == newDelegatee || token.delegatesVotesCount(address(this), oldDelegatee) == 0);
-        require(token.delegatesVotesCount(address(this), newDelegatee) == expected);
+        require(
+            oldDelegatee == newDelegatee ||
+                token.delegatesVotesCount(address(this), oldDelegatee) == 0
+        );
+        require(
+            token.delegatesVotesCount(address(this), newDelegatee) == expected
+        );
         require(token.userDelegatedVotes(address(this)) == expected);
         require(token.getVotes(newDelegatee) == expected);
         require(token.freeVotes(address(this)) == expectedFree);
@@ -219,7 +243,14 @@ contract ERC20MultiVotesUnitTest is Test {
                 abi.encodePacked(
                     "\x19\x01",
                     token.DOMAIN_SEPARATOR(),
-                    keccak256(abi.encode(token.DELEGATION_TYPEHASH(), newDelegatee, 0, block.timestamp))
+                    keccak256(
+                        abi.encode(
+                            token.DELEGATION_TYPEHASH(),
+                            newDelegatee,
+                            0,
+                            block.timestamp
+                        )
+                    )
                 )
             )
         );
@@ -228,7 +259,10 @@ contract ERC20MultiVotesUnitTest is Test {
         uint256 expectedFree = newDelegatee == address(0) ? mintAmount : 0;
 
         token.delegateBySig(newDelegatee, 0, block.timestamp, v, r, s);
-        require(oldDelegatee == newDelegatee || token.delegatesVotesCount(owner, oldDelegatee) == 0);
+        require(
+            oldDelegatee == newDelegatee ||
+                token.delegatesVotesCount(owner, oldDelegatee) == 0
+        );
         require(token.delegatesVotesCount(owner, newDelegatee) == expected);
         require(token.userDelegatedVotes(owner) == expected);
         require(token.getVotes(newDelegatee) == expected);
@@ -247,7 +281,10 @@ contract ERC20MultiVotesUnitTest is Test {
 
         uint256 block1 = block.number;
         require(token.numCheckpoints(delegate1) == 1);
-        ERC20MultiVotes.Checkpoint memory checkpoint1 = token.checkpoints(delegate1, 0);
+        ERC20MultiVotes.Checkpoint memory checkpoint1 = token.checkpoints(
+            delegate1,
+            0
+        );
         require(checkpoint1.fromBlock == block1);
         require(checkpoint1.votes == 4e18);
 
@@ -274,7 +311,10 @@ contract ERC20MultiVotesUnitTest is Test {
         require(checkpoint1.votes == 8e18);
 
         // new checkpoint 2
-        ERC20MultiVotes.Checkpoint memory checkpoint2 = token.checkpoints(delegate1, 1);
+        ERC20MultiVotes.Checkpoint memory checkpoint2 = token.checkpoints(
+            delegate1,
+            1
+        );
         require(checkpoint2.fromBlock == block2);
         require(checkpoint2.votes == 6e18);
 
@@ -298,7 +338,10 @@ contract ERC20MultiVotesUnitTest is Test {
         require(checkpoint2.votes == 6e18);
 
         // new checkpoint 3
-        ERC20MultiVotes.Checkpoint memory checkpoint3 = token.checkpoints(delegate1, 2);
+        ERC20MultiVotes.Checkpoint memory checkpoint3 = token.checkpoints(
+            delegate1,
+            2
+        );
         require(checkpoint3.fromBlock == block3);
         require(checkpoint3.votes == 10e18);
 
