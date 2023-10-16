b900f95c90fd59a3747a35e6f487abd4d87d7b2e
Elliot <elliotfriedman3@gmail.com>
2023-10-16 11:36:34 -0700

    borrow repay tests
    Signed-off-by: Elliot <elliotfriedman3@gmail.com>

--- test/integration/proposal-checks/IntegrationTestBorrowSDAICollateral.sol
+++ test/integration/proposal-checks/IntegrationTestBorrowSDAICollateral.sol
@@ -19,10 +19,125 @@ import {LendingTermOnboarding} from "@src/governance/LendingTermOnboarding.sol";
 import {LendingTermOffboarding} from "@src/governance/LendingTermOffboarding.sol";
 import {ProtocolConstants as constants} from "@test/utils/ProtocolConstants.sol";
 
-contract IntegrationTestBorrowSDAICollateral is PostProposalCheck {
-    function setUp() public override {
-        super.setUp();
+contract IntegrationTestBorrowAgainstSDAICollateral is PostProposalCheck {
+    function testTermParamSetup() public {
+        assertEq(term.collateralToken(), address(sdai));
+        LendingTerm.LendingTermParams memory params = term.getParameters();
+        assertEq(params.collateralToken, address(sdai));
+        assertEq(params.openingFee, 0);
+        assertEq(params.interestRate, constants.SDAI_RATE);
+        assertEq(params.minPartialRepayPercent, 0);
+        assertEq(params.maxDelayBetweenPartialRepay, 0);
+        assertEq(
+            params.maxDebtPerCollateralToken,
+            constants.MAX_SDAI_CREDIT_RATIO
+        );
     }
 
-    /// TODO
+    function testVoteForSDAIGauge() public {
+        /// setup
+        vm.prank(addresses.mainnet(strings.TEAM_MULTISIG));
+        rateLimitedGuildMinter.mint(address(this), constants.GUILD_SUPPLY); /// mint all of the guild to this contract
+        guild.delegate(address(this));
+
+        assertTrue(guild.isGauge(address(term)));
+        assertEq(guild.numGauges(), 1);
+        assertEq(guild.numLiveGauges(), 1);
+    }
+
+    function testAllocateGaugeToSDAI() public {
+        testVoteForSDAIGauge();
+
+        guild.incrementGauge(address(term), constants.GUILD_SUPPLY);
+
+        assertEq(guild.totalWeight(), constants.GUILD_SUPPLY);
+        assertTrue(guild.isUserGauge(address(this), address(term)));
+    }
+
+    function testSupplyCollateralUserOne(uint128 supplyAmount) public returns (bytes32 loanId) {
+        supplyAmount = uint128(
+            _bound(
+                uint256(supplyAmount),
+                term.MIN_BORROW(),
+                constants.CREDIT_HARDCAP - constants.CREDIT_SUPPLY
+            )
+        );
+
+        testAllocateGaugeToSDAI();
+
+        deal(address(sdai), userOne, supplyAmount);
+
+        vm.startPrank(userOne);
+        sdai.approve(address(term), supplyAmount);
+        loanId = term.borrow(supplyAmount, supplyAmount);
+        vm.stopPrank();
+
+        assertEq(term.getLoanDebt(loanId), supplyAmount, "incorrect loan debt");
+        assertEq(
+            sdai.balanceOf(address(term)),
+            supplyAmount,
+            "sdai balance of term incorrect"
+        );
+        assertEq(
+            credit.totalSupply(),
+            supplyAmount + constants.CREDIT_SUPPLY,
+            "incorrect credit supply"
+        );
+        assertEq(
+            credit.balanceOf(userOne),
+            supplyAmount,
+            "incorrect credit balance"
+        );
+        assertEq(
+            rateLimitedCreditMinter.buffer(),
+            constants.CREDIT_HARDCAP - constants.CREDIT_SUPPLY - supplyAmount,
+            "incorrect buffer"
+        );
+        assertEq(
+            rateLimitedCreditMinter.lastBufferUsedTime(),
+            block.timestamp,
+            "incorrect last buffer used time"
+        );
+    }
+
+    function testRepayLoan(uint128 supplyAmount) public {
+        bytes32 loanId = testSupplyCollateralUserOne(supplyAmount);
+        vm.warp(block.timestamp + 1);
+
+        /// account for accrued interest
+        deal(address(credit), userOne, term.getLoanDebt(loanId));
+
+        vm.startPrank(userOne);
+        credit.approve(address(term), term.getLoanDebt(loanId));
+        term.repay(loanId);
+        vm.stopPrank();
+
+        assertEq(term.getLoanDebt(loanId), 0, "incorrect loan debt");
+
+        assertEq(
+            sdai.balanceOf(address(term)),
+            0,
+            "sdai balance of term incorrect"
+        );
+        assertEq(
+            credit.totalSupply(),
+            constants.CREDIT_SUPPLY,
+            "incorrect credit supply"
+        );
+        assertEq(
+            credit.balanceOf(userOne),
+            0,
+            "incorrect credit balance"
+        );
+        assertEq(
+            rateLimitedCreditMinter.buffer(),
+            constants.CREDIT_HARDCAP - constants.CREDIT_SUPPLY,
+            "incorrect buffer"
+        );
+        assertEq(
+            rateLimitedCreditMinter.lastBufferUsedTime(),
+            block.timestamp,
+            "incorrect last buffer used time"
+        );
+    }
 }
