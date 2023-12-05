92276d5d8016f21a0918097bf21f54c0073e041a
Erwan Beauvois <eswak@users.noreply.github.com>
2023-12-05 16:13:30 +0100

    Fix Proposal_0 roles granting & remove dead code

--- test/proposals/Proposal_0.sol
+++ test/proposals/Proposal_0.sol
@@ -44,14 +44,8 @@ contract Proposal_0 is Proposal {
     /// the Surplus Guild Minter
     uint256 internal constant GUILD_CREDIT_REWARD_RATIO = 0.1e18;
 
-    /// @notice initial credit supply is 100 tokens after USDC PSM mint
-    uint256 internal constant CREDIT_SUPPLY = 100 * 1e18;
-
-    /// @notice initial amount of USDC to mint with is 100
-    uint256 internal constant INITIAL_USDC_MINT_AMOUNT = 100 * 1e6;
-
     /// @notice maximum delegates for both credit and guild token
-    uint256 internal constant MAX_DELEGATES = 12;
+    uint256 internal constant MAX_DELEGATES = 10;
 
     /// @notice for each SDAI collateral, up to 1 credit can be borrowed
     uint256 internal constant MAX_SDAI_CREDIT_RATIO = 1e18;
@@ -67,7 +61,7 @@ contract Proposal_0 is Proposal {
     /// ------------------------------------------------------------------------
 
     /// @notice rate to borrow against SDAI collateral
-    uint256 internal constant SDAI_RATE = 0.03e18;
+    uint256 internal constant SDAI_RATE = 0.04e18;
 
     /// ------------------------------------------------------------------------
     /// @notice profit sharing configuration parameters for the Profit Manager
@@ -84,6 +78,7 @@ contract Proposal_0 is Proposal {
 
     /// @notice 0% of profits go to other
     uint256 internal constant OTHER_SPLIT = 0;
+    address internal constant OTHER_ADDRESS = address(0);
 
     uint256 public constant BLOCKS_PER_DAY = 7164;
 
@@ -304,24 +299,12 @@ contract Proposal_0 is Proposal {
             address _lendingTermV1 = addresses.mainnet("LENDING_TERM_V1");
             termOnboarding.allowImplementation(_lendingTermV1, true);
 
-            address termUSDC1 = termOnboarding.createTerm(
-                _lendingTermV1,
-                LendingTerm.LendingTermParams({
-                    collateralToken: addresses.mainnet("ERC20_USDC"),
-                    maxDebtPerCollateralToken: 1e30, // 1 CREDIT per USDC collateral + 12 decimals correction
-                    interestRate: 0, // 0%
-                    maxDelayBetweenPartialRepay: 0, // no periodic partial repay needed
-                    minPartialRepayPercent: 0, // no minimum size for partial repay
-                    openingFee: 0, // 0%
-                    hardCap: CREDIT_HARDCAP // max 2m CREDIT issued
-                })
-            );
             address termSDAI1 = termOnboarding.createTerm(
                 _lendingTermV1,
                 LendingTerm.LendingTermParams({
                     collateralToken: addresses.mainnet("ERC20_SDAI"),
                     maxDebtPerCollateralToken: 1e18, // 1 CREDIT per SDAI collateral + no decimals correction
-                    interestRate: SDAI_RATE, // 3%
+                    interestRate: SDAI_RATE, // 4%
                     maxDelayBetweenPartialRepay: 0, // no periodic partial repay needed
                     minPartialRepayPercent: 0, // no minimum size for partial repay
                     openingFee: 0, // 0%
@@ -331,7 +314,6 @@ contract Proposal_0 is Proposal {
 
             addresses.addMainnet("TERM_IMPL", _lendingTermV1);
             addresses.addMainnet("TERM_SDAI_1", termSDAI1);
-            addresses.addMainnet("TERM_USDC_1", termUSDC1);
         }
     }
 
@@ -361,10 +343,6 @@ contract Proposal_0 is Proposal {
         core.grantRole(CoreRoles.CREDIT_MINTER, addresses.mainnet("PSM_USDC"));
 
         // RATE_LIMITED_CREDIT_MINTER
-        core.grantRole(
-            CoreRoles.RATE_LIMITED_CREDIT_MINTER,
-            addresses.mainnet("TERM_USDC_1")
-        );
         core.grantRole(
             CoreRoles.RATE_LIMITED_CREDIT_MINTER,
             addresses.mainnet("TERM_SDAI_1")
@@ -466,10 +444,6 @@ contract Proposal_0 is Proposal {
 
         core.grantRole(
             CoreRoles.TIMELOCK_CANCELLER,
-            addresses.mainnet("TEAM_MULTISIG")
-        );
-        core.grantRole(
-            CoreRoles.TIMELOCK_CANCELLER,
             addresses.mainnet("DAO_VETO_CREDIT")
         );
         core.grantRole(
@@ -488,6 +462,10 @@ contract Proposal_0 is Proposal {
             CoreRoles.TIMELOCK_CANCELLER,
             addresses.mainnet("DAO_GOVERNOR_GUILD")
         );
+        core.grantRole(
+            CoreRoles.TIMELOCK_CANCELLER,
+            addresses.mainnet("ONBOARD_GOVERNOR_GUILD")
+        );
 
         // Configuration
         ProfitManager(addresses.mainnet("PROFIT_MANAGER")).initializeReferences(
@@ -501,7 +479,7 @@ contract Proposal_0 is Proposal {
                 CREDIT_SPLIT, // 90% creditSplit
                 GUILD_SPLIT, // 1% guildSplit
                 OTHER_SPLIT, // otherSplit
-                address(0) // otherRecipient
+                OTHER_ADDRESS // otherRecipient
             );
         GuildToken(addresses.mainnet("ERC20_GUILD")).setCanExceedMaxGauges(
             addresses.mainnet("SURPLUS_GUILD_MINTER"),
@@ -878,7 +856,7 @@ contract Proposal_0 is Proposal {
             );
             assertEq(
                 otherRecipient,
-                address(0),
+                OTHER_ADDRESS,
                 "Profit Manager other recipient incorrect"
             );
         }
