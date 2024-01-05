c4e393a10ba7065473ee3d62fdff13edf640248d
Alfu <Alfulinku@gmail.com>
2024-01-05 15:58:07 +0100

    use canCall from the factory and add simple withdraw function for any tokens

--- src/account/AccountImplementation.sol
+++ src/account/AccountImplementation.sol
@@ -6,6 +6,7 @@ import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {BoringBatchable} from "@src/account/BoringBatchable.sol";
 
 import {AccountFactory} from "@src/account/AccountFactory.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 /// Smart account implementation for the ECG
 contract AccountImplementation is BoringBatchable, Ownable {
@@ -26,34 +27,23 @@ contract AccountImplementation is BoringBatchable, Ownable {
         revert("AccountImplementation: cannot renounce ownership");
     }
 
-    // function to perform external calls (can be batched)
+    /// @notice allows to withdraw any token from the account contract
+    function withdraw(address token, address receiver, uint256 amount) public onlyOwner {
+        IERC20(token).transfer(receiver, amount);
+    }
+
+    /// @notice function to perform external calls (can be batched)
     function callExternal(
         address target,
         bytes calldata data
     ) public onlyOwner {
-        /*bytes4 sig = uint32(bytes32(data));
-        require(AccountFactory(factory).canCall(target, data), "AccountImplementation: cannot call target");*/
+        // Extract the function selector from the first 4 bytes of `data`
+        bytes4 functionSelector = bytes4(data[:4]);
+        require(AccountFactory(factory).canCall(target, functionSelector), "AccountImplementation: cannot call target");
 
         (bool success, bytes memory result) = target.call(data);
         if (!success) {
             _getRevertMsg(result);
         }
-
-        /*
-        for (uint256 i = 0; i < calls.length; i++) {
-            (bool success, bytes memory result) = address(this).delegatecall(calls[i]);
-            if (!success && revertOnFail) {
-                _getRevertMsg(result);
-            }
-        }
-         */
     }
-
-    /*monAccount.batch([
-        abi.encodeWithSelector(
-            AccountImplementation.callExternal.sig,
-            curvePool
-            abi.encodeWithSignature("swap(uint256,uint256)", amountIn, minAmountOut)
-        )
-    ])*/
 }
