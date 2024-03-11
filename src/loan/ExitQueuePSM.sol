81d4cead18a862ec5f87ebc3e631ca37b0b5ed8e
Alfu <Alfulinku@gmail.com>
2024-03-11 14:35:55 +0100

    cleanup imports 2

--- src/loan/ExitQueuePSM.sol
+++ src/loan/ExitQueuePSM.sol
@@ -2,10 +2,9 @@
 pragma solidity 0.8.13;
 
 import {SimplePSM} from "@src/loan/SimplePSM.sol";
-import {DoubleEndedQueue} from "@openzeppelin/contracts/utils/structs/DoubleEndedQueue.sol";
-
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {DoubleEndedQueue} from "@openzeppelin/contracts/utils/structs/DoubleEndedQueue.sol";
 
 /// @notice Variation of the SimplePSM contract that allow users holding CREDIT to enter in the exit queue
 /// to have their CREDIT automatically redeemed when another user mint some CREDIT
