d7f856ace533305ed96867c7fdfb7f201d8969a1
Erwan Beauvois <eswak@users.noreply.github.com>
2024-04-02 12:14:55 +0200

    Add string.contains util function in ECGTest.sol

--- test/ECGTest.sol
+++ test/ECGTest.sol
@@ -145,4 +145,28 @@ abstract contract ECGTest is Test {
         );
         _write(newAddresses);
     }
+
+    function _contains(string memory what, string memory where) internal pure returns (bool) {
+        bytes memory whatBytes = bytes(what);
+        bytes memory whereBytes = bytes(where);
+
+        if (whatBytes.length > whereBytes.length) {
+            return false;
+        }
+
+        bool found = false;
+        for (uint i = 0; i <= whereBytes.length - whatBytes.length; i++) {
+            bool flag = true;
+            for (uint j = 0; j < whatBytes.length; j++)
+                if (whereBytes [i + j] != whatBytes [j]) {
+                    flag = false;
+                    break;
+                }
+            if (flag) {
+                found = true;
+                break;
+            }
+        }
+        return found;
+    }
 }
