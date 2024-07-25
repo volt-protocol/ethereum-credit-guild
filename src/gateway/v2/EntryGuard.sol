8118724993aa11d346f2a14da743a06b4fbd0745
Erwan Beauvois <eswak@users.noreply.github.com>
2024-07-25 13:23:07 +0200

    Remove OZ StorageSLot & add TStorageLib

--- src/gateway/v2/EntryGuard.sol
+++ src/gateway/v2/EntryGuard.sol
@@ -1,29 +1,35 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.24;
 
-import {StorageSlot} from "./StorageSlot.sol";
+import {TStorageLib} from "@src/gateway/v2/TStorageLib.sol";
 
 /// @title Util contract to manage reentrancy
 abstract contract EntryGuard {
-    using StorageSlot for *;
 
     // keccak256(abi.encode(uint256(keccak256("ecg.storage.gateway.originalSender")) - 1)) & ~bytes32(uint256(0xff))
-    bytes32 private constant _SLOT_ORIGINAL_SENDER =
-        keccak256(abi.encode(uint256(keccak256("ecg.storage.gateway.originalSender")) - 1)) & ~bytes32(uint256(0xff));
+    bytes32 private constant _SLOT_ORIGINAL_SENDER = 0x6693a246d58d32f385e51820a8b8b6ff5c374d7a4f80507dca16a8f0445e2800;
+
+    function _getOriginalSender() internal view returns (address) {
+        return TStorageLib._address(_SLOT_ORIGINAL_SENDER);
+    }
+
+    function _setOriginalSender(address v) internal {
+        TStorageLib._address(_SLOT_ORIGINAL_SENDER, v);
+    }
 
     modifier entryPoint() {
         require(
-            _SLOT_ORIGINAL_SENDER.asAddress().tload() == address(0),
+            _getOriginalSender() == address(0),
             "EntryGuard: original sender already set"
         );
-        _SLOT_ORIGINAL_SENDER.asAddress().tstore(msg.sender);
+        _setOriginalSender(msg.sender);
         _;
-        _SLOT_ORIGINAL_SENDER.asAddress().tstore(address(0));
+        _setOriginalSender(address(0));
     }
 
     modifier afterEntry() {
         require(
-            _SLOT_ORIGINAL_SENDER.asAddress().tload() != address(0),
+            _getOriginalSender() != address(0),
             "EntryGuard: originalSender not set"
         );
         _;
